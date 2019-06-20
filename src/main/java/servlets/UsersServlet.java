344ab161faa7f00329301bc4698e08638b3a9c16
dropyourtune <pedchenko.dmitry@gmail.com>
2019-06-20 23:00:23 +0300

    add users action

--- src/main/java/servlets/UsersServlet.java
+++ src/main/java/servlets/UsersServlet.java
@@ -1,6 +1,9 @@
 package servlets;
 
 import accounts.AccountService;
+import accounts.UserProfile;
+import com.google.gson.Gson;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class UsersServlet extends HttpServlet {
+    private String contentType = "text/html;charset=utf-8";
     private final AccountService accountService;
 
     public UsersServlet(AccountService accountService) {
@@ -16,12 +20,34 @@ public class UsersServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws ServletException, IOException {
-
+        response.setContentType(contentType);
+        Gson gson = new Gson();
+        response.getWriter().println(gson.toJson(accountService));
+        response.setStatus(HttpServletResponse.SC_OK);
     }
 
     public void doPost(HttpServletRequest request,
                   HttpServletResponse response) throws ServletException, IOException {
-        
+        String login = request.getParameter("loginSignUp");
+        String pass = request.getParameter("passwordSignUp");
+        String email = request.getParameter("email");
+
+        if (login == null || pass == null) {
+            response.setContentType(contentType);
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        UserProfile userProfile = accountService.getUserByLogin(login);
+        if (userProfile != null) {
+            response.setContentType(contentType);
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        accountService.addNewUser(new UserProfile(login, pass, email));
+        response.setContentType(contentType);
+        response.setStatus(HttpServletResponse.SC_OK);
     }
 
     public void doPut(HttpServletRequest request,
